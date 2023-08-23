035a72d050d369607b3084775ffbd5e8d76ad26c
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-22 23:55:35 -0700

    Update

--- merging2SortedLists/mergingTwoSortedLists.py
+++ merging2SortedLists/mergingTwoSortedLists.py
@@ -1,12 +1,13 @@
+from typing import Optional
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-from typing import Optional
-
 class Solution:
+    # Solution using linked lists
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         cur = dummy = ListNode()
 
