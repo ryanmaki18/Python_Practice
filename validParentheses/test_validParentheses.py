a5ffa852d60e5512f07d9af5fbd63184abc7e76d
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-22 23:32:27 -0700

    Updating testcase

--- validParentheses/test_validParentheses.py
+++ validParentheses/test_validParentheses.py
@@ -4,23 +4,23 @@ import unittest
 class ValidParentheses(unittest.TestCase):
     def test_example_1(self):
         solution = Solution()
-        self.assertTrue(solution.isValid("()"), True)
+        self.assertTrue(solution.isValid("()"))
         
     def test_example_2(self):
         solution = Solution()
-        self.assertTrue(solution.isValid("()[]{}"), True)
+        self.assertTrue(solution.isValid("()[]{}"))
         
     def test_example_3(self):
         solution = Solution()
-        self.assertTrue(solution.isValid("([{}])"), True)
+        self.assertTrue(solution.isValid("([{}])"))
         
     def test_example43(self):
         solution = Solution()
-        self.assertFalse(solution.isValid("(]"), False)
+        self.assertFalse(solution.isValid("(]"))
 
     def test_example_5(self):
         solution = Solution()
-        self.assertFalse(solution.isValid("([{}])]"), False)
+        self.assertFalse(solution.isValid("([{}])]"))
 
 if __name__ == '__main__':
     unittest.main()
