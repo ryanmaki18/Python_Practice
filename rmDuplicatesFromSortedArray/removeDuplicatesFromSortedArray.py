ccce9a8fef68dd4aa32597bd5a7ed83729f0abdb
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-23 00:07:18 -0700

    Update

--- rmDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.py
+++ rmDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.py
@@ -1,3 +1,12 @@
+# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each 
+# unique element appears only once. The relative order of the elements should be kept the same. Then return the 
+# number of unique elements in nums. 
+
+# Constraints:
+    # 1 <= nums.length <= 3 * 104
+    # -100 <= nums[i] <= 100
+    # nums is sorted in non-decreasing order.
+
 
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
--- rmDuplicatesFromSortedArray/test_removeDuplicatesFromSortedArray.py
+++ rmDuplicatesFromSortedArray/test_removeDuplicatesFromSortedArray.py
@@ -1,26 +1,18 @@
 from removeDuplicatesFromSortedArray import Solution
 import unittest
 
-class ValidParentheses(unittest.TestCase):
+class RemoveDuplicatesFromSortedArray(unittest.TestCase):
     def test_example_1(self):
         solution = Solution()
-        self.assertTrue(solution.isValid("()"))
+        given_input = [1,1,2]
+        output = "2, [1,2,_]"
+        self.assertEqual(solution.removeDuplicates(given_input), output)
         
     def test_example_2(self):
         solution = Solution()
-        self.assertTrue(solution.isValid("()[]{}"))
-        
-    def test_example_3(self):
-        solution = Solution()
-        self.assertTrue(solution.isValid("([{}])"))
-        
-    def test_example43(self):
-        solution = Solution()
-        self.assertFalse(solution.isValid("(]"))
-
-    def test_example_5(self):
-        solution = Solution()
-        self.assertFalse(solution.isValid("([{}])]"))
+        given_input = [0,0,1,1,1,2,2,3,3,4]
+        output = "5, [0,1,2,3,4,_,_,_,_,_]"
+        self.assertEqual(solution.removeDuplicates(given_input), output)
 
 if __name__ == '__main__':
     unittest.main()
