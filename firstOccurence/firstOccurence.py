be0e54ab83b433610ad3678fbcbacb1e1bd59f4f
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-24 11:43:19 -0700

    Added 3 implementations of firstOccurence

--- firstOccurence/firstOccurence.py
+++ firstOccurence/firstOccurence.py
@@ -1,6 +1,51 @@
+
+# ------- Three implementations, , a built-in, brute force, and using the KPM algorithm -------
 class Solution:
-    # Two implementations, brute force, and using the KPM algorithm
-    
+    # ------- Easiest Solution (using built-in find function). Is O(n) -------
+    """
     def strStr(self, haystack: str, needle: str) -> int:
-        # Implement Brute force first....
+        return haystack.find(needle)
+    """
+
+
+    # ------- Brute Force -------
+    """
+    def strStr(self, haystack: str, needle: str) -> int:
+        N, H = len(needle), len(haystack)
+
+        for i in range(H - N + 1):
+            for j in range(N):
+                if needle[j] != haystack[i + j]:
+                    break
+                if j == N - 1:
+                    return i
+                
+        return -1  
+    """
+
+    # ------- Using KPM algorithm -------
+    def strStr(self, haystack: str, needle: str) -> int:
+        lps = [0] * len(needle)
+
+        # Preprocessing
+        pre = 0
+        for i in range(1, len(needle)):
+            while (pre > 0 and
+                    needle[i] != needle[pre]):
+                pre = lps[pre-1]
+            if needle[pre] == needle[i]:
+                pre += 1
+                lps[i] = pre
+
+        # Main algorithm
+        n = 0 #needle index
+        for h in range(len(haystack)):
+            while (n > 0 and needle[n] != haystack[h]):
+                n = lps[n-1]
+            if needle[n] == haystack[h]:
+                n += 1
+            if n == len(needle):
+                return h - n + 1
+
+        return -1
         
--- firstOccurence/test_firstOccurence.py
+++ firstOccurence/test_firstOccurence.py
@@ -7,28 +7,28 @@ class FirstOccurenceInString(unittest.TestCase):
         haystack = "sadbutsad"
         needle = "sad"
         output = 0
-        self.assertEqual(solution.removeElement(haystack, needle), output)
+        self.assertEqual(solution.strStr(haystack, needle), output)
         
     def test_example_2(self):
         solution = Solution()
         haystack = "dndhdhjdhRYAN"
         needle = "RYAN"
         output = 9              # Check if correct index later
-        self.assertEqual(solution.removeElement(haystack, needle), output)
+        self.assertEqual(solution.strStr(haystack, needle), output)
         
     def test_example_3(self):
         solution = Solution()
         haystack = "leetcode"
         needle = "leeto"
         output = -1
-        self.assertEqual(solution.removeElement(haystack, needle), output)
+        self.assertEqual(solution.strStr(haystack, needle), output)
     
     def test_example_4(self):
         solution = Solution()
         haystack = "randomstring"
         needle = "qqq"
         output = -1
-        self.assertEqual(solution.removeElement(haystack, needle), output)
+        self.assertEqual(solution.strStr(haystack, needle), output)
         
 
 if __name__ == '__main__':
