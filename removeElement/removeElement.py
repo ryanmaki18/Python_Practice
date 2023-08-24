c320adb3714232d31bfc00b180aaa73f2fd30bca
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-23 23:31:55 -0700

    Updated Testcases

--- removeElement/removeElement.py
+++ removeElement/removeElement.py
@@ -1,5 +1,14 @@
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
+        if not nums:
+            return 0
         
+        k = 1
+        for num in range(1, len(nums)):
+            if nums[num] != nums[k - 1]:
+                nums[k] = nums[num]
+                k += 1
+        
+        return k
         
         
--- removeElement/test_removeElement.py
+++ removeElement/test_removeElement.py
@@ -4,9 +4,17 @@ import unittest
 class ValidParentheses(unittest.TestCase):
     def test_example_1(self):
         solution = Solution()
-        given_input = [1,1,2]
+        given_input = [3,2,2,3]
         output = 2
-        self.assertEqual(solution.removeElement(given_input), output)
+        val = 3
+        self.assertEqual(solution.removeElement(given_input), output, val)
+        
+    def test_example_1(self):
+        solution = Solution()
+        given_input = [0,1,2,2,3,0,4,2]
+        output = 5
+        val = 2
+        self.assertEqual(solution.removeElement(given_input), output, val)
         
 
 
