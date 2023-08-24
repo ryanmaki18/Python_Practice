c500c1ed3e1906acb49623d4682c7f87c17dfbe8
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-24 00:01:07 -0700

    Updated testcases

--- removeElement/test_removeElement.py
+++ removeElement/test_removeElement.py
@@ -5,22 +5,22 @@ class RemoveElement(unittest.TestCase):
     def test_example_1(self):
         solution = Solution()
         given_input = [3,2,2,3]
-        output = 2
         val = 3
+        output = 2
         self.assertEqual(solution.removeElement(given_input, val), output)
         
     def test_example_2(self):
         solution = Solution()
         given_input = [0,1,2,2,3,0,4,2]
-        output = 5
         val = 2
+        output = 5
         self.assertEqual(solution.removeElement(given_input, val), output)
         
-    def test_example_3(self):
+    def test_example_all_same(self):
         solution = Solution()
         given_input = [0,0,0,0]
-        output = 0
         val = 0
+        output = 0
         self.assertEqual(solution.removeElement(given_input, val), output)
         
 
