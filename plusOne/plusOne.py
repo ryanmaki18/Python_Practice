572483503a68c80311391ace6a329e71ceaa5ad9
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-24 16:32:20 -0700

    Finished plusOne

--- plusOne/plusOne.py
+++ plusOne/plusOne.py
@@ -1,3 +1,17 @@
 class Solution:
+    # Without a for loop
     def plusOne(self, digits: list[int]) -> list[int]:
-        
+        s = ''.join(map(str,digits))
+        i = int(s) + 1
+        li = list(map(int, str(i)))
+        return li
+
+    # # With a for loop
+    # def plusOne(self, digits: list[int]) -> list[int]:
+    #     for i in range(len(digits)-1, -1, -1):
+    #         if digits[i] == 9:
+    #             digits[i] = 0
+    #         else:
+    #             digits[i] += 1
+    #             return digits
+    #     return [1] + digits
