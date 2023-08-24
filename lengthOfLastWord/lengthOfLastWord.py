12367625f7c4ea20550f236116b9f217fa3b7440
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-24 12:26:08 -0700

    Finished lengthOfLastWord

--- lengthOfLastWord/lengthOfLastWord.py
+++ lengthOfLastWord/lengthOfLastWord.py
@@ -1,4 +1,9 @@
 class Solution:
+    # ------ Easiest Solution ------
+    # def lengthOfLastWord(self, s: str) -> int:
+    #     res=s.split()
+    #     return len(res[-1])
+
+    ## ------ Can be done in one line as well. Not as readable. ------
     def lengthOfLastWord(self, s: str) -> int:
-        
-        
+        return len(s.split()[-1])
