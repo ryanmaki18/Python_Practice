21c03c51c17a873fe581cf434345e130cc1f0bb9
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-24 12:10:58 -0700

    Implemented binary search logic in searchInsert. Making brute force next.

--- searchInsert/searchInsert.py
+++ searchInsert/searchInsert.py
@@ -1,5 +1,25 @@
 class Solution:
+    # def searchInsert(self, nums: List[int], target: int) -> int:
+    #     for i in range(len(nums)):
+    #         if nums[i] == target:
+    #             return i
+    #     index = 0
+    #     for i in range(len(nums)):
+    #         if nums[i] < target:
+    #             continue
+    #         else:
+    #             return i
+
+
+# -------- Binary Search Logic. O(log n)--------
     def searchInsert(self, nums: List[int], target: int) -> int:
-        
-        
-        
+        left,right=0,len(nums)-1
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                left=mid+1
+            else:
+                right=mid-1
+        return left
