54d22494bead8d710fd5765a6b927702ebd00204
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-28 23:38:14 -0700

    Updated Testcases

--- mergeSortedArray/mergeSortedArray.py
+++ mergeSortedArray/mergeSortedArray.py
@@ -1,8 +1,8 @@
+
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        
         
         
--- mergeSortedArray/test_mergeSortedArray.py
+++ mergeSortedArray/test_mergeSortedArray.py
@@ -4,9 +4,30 @@ import unittest
 class MergeSortedList(unittest.TestCase):
     def test_example_1(self):
         solution = Solution()
-        s = "Hello World"
-        output = 5
-        self.assertEqual(solution.merge(s), output)         ## FIXME: Not updated testcase
+        nums1 = [1,2,3,0,0,0]
+        m = 3
+        nums2 = [2,5,6]
+        n = 3
+        output: [1,2,2,3,5,6]
+        self.assertEqual(solution.merge(nums1, m, nums2, n), output)
+        
+    def test_example_2(self):
+        solution = Solution()
+        nums1 = [1]
+        m = 1
+        nums2 = []
+        n = 0
+        output: [1]
+        self.assertEqual(solution.merge(nums1, m, nums2, n), output)
+        
+    def test_example_3(self):
+        solution = Solution()
+        nums1 = [0]
+        m = 0
+        nums2 = [1]
+        n = 1
+        output: [1]
+        self.assertEqual(solution.merge(nums1, m, nums2, n), output)
 
         
 if __name__ == '__main__':
