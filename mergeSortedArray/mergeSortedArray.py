efd4a26f351d5f854e714b44a9aa4645c4c78618
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-28 23:45:30 -0700

    working on leetcode, must fix testcases. going to sleep

--- mergeSortedArray/mergeSortedArray.py
+++ mergeSortedArray/mergeSortedArray.py
@@ -4,5 +4,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        
+        for j in range(n):
+            nums1[m+j] = nums2[j]
+        nums1.sort()
         
