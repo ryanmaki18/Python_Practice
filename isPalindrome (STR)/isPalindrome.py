09a077d4f8b3ba5151fbb82a99f4bf649a2cded7
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-30 14:39:35 -0700

    Finished isPalindrome w/ 2 implementations

--- isPalindrome (STR)/isPalindrome.py
+++ isPalindrome (STR)/isPalindrome.py
@@ -1,11 +1,24 @@
 class Solution:
+    # def isPalindrome(self, s: str) -> bool:
+    #     result = ''
+    #     for char in s:
+    #         if char.isalpha():
+    #             result += char.lower()
+    #         if char.isdigit():
+    #             result += char
+    #     if result==result[-1::-1]:
+    #         return True
+    #     else:
+    #         return False
+
     def isPalindrome(self, s: str) -> bool:
-        result = ''
-        for char in s:
-            if char.isalpha():
-                result += char.islower()
-            if char.isdigit():
-                result += char
-        if result == reversed(result):
-            return True
-        return False
+        cleaned = ''.join(char.lower() for char in s if char.isalnum())
+        left, right = 0, len(cleaned) - 1
+        
+        while left < right:
+            if cleaned[left] != cleaned[right]:
+                return False
+            left += 1
+            right -= 1
+        
+        return True
