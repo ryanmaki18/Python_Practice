caffddedf3d88351ae09f593873ab00c492e4374
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-29 17:14:21 -0700

    updated rmduplicatedfromsortedlist

--- rmDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.py
+++ rmDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.py
@@ -14,9 +14,9 @@ class Solution:
             return 0
         
         k = 1
-        for num in range(1, len(nums)):
-            if nums[num] != nums[k - 1]:
-                nums[k] = nums[num]
+        for i in range(1, len(nums)):
+            if nums[i] != nums[k - 1]:
+                nums[k] = nums[i]
                 k += 1
         
         return k
