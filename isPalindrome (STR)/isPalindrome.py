5c6fdeee64df51cf210044c7ff7672b479700b45
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-30 14:03:42 -0700

    update

--- isPalindrome (STR)/isPalindrome.py
+++ isPalindrome (STR)/isPalindrome.py
@@ -1,8 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        if len(s) < 0:
-            return False
-        
-        reversed_str = ''.join(reversed(s))
-        return s == reversed_str
-    
+        result = ''
+        for char in s:
+            if char.isalpha():
+                result += char.islower()
+            if char.isdigit():
+                result += char
+        if result == reversed(result):
+            return True
+        return False
