50321be8957a286a823dca15341d1ff81ad6368c
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-30 17:41:22 -0700

    Finished extra implementations of validAnagram

--- validAnagram/validAnagram.py
+++ validAnagram/validAnagram.py
@@ -1,11 +1,36 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+from collections import Counter
 
-        for idx in set(s):
-            # Compare s.count(l) and t.count(l) for every index i from 0 to 26...
-            # If any idx count is different, return false...
-            if s.count(idx) != t.count(idx):
+class Solution:
+    # def isAnagram(self, s: str, t: str) -> bool:
+    #     if len(s) != len(t):
+    #         return False
+
+    #     for i in set(s):
+    #         # Compare s.count(l) and t.count(l) for every index i from 0 to 26
+    #         # If any idx count is different, return false
+    #         if s.count(i) != t.count(i):
+    #             return False
+    #     return True   
+    
+    # Using built-in counter function
+    def isAnagram(self, s: str, t: str) -> bool:
+        sCount, tCount = Counter(s), Counter(t)
+        return sCount == tCount
+    
+    # Without using built-in counter function
+    def isAnagram(self, s: str, t: str) -> bool:
+            if len(s) != len(t):
                 return False
-        return True   
+        
+            counter_s = [0] * 26
+            counter_t = [0] * 26
+        
+            for char in s:
+                counter_s[ord(char) - ord('a')] += 1
+        
+            for char in t:
+                counter_t[ord(char) - ord('a')] += 1
+            
+            return counter_s == counter_t
+    
+    
