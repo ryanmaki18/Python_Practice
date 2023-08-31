828ef32193bc74cd7a5e83870f04c7967121a63f
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-30 17:23:54 -0700

    finished isAnagram

--- validAnagram/validAnagram.py
+++ validAnagram/validAnagram.py
@@ -1,3 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
+        if len(s) != len(t):
+            return False
+
+        for idx in set(s):
+            # Compare s.count(l) and t.count(l) for every index i from 0 to 26...
+            # If any idx count is different, return false...
+            if s.count(idx) != t.count(idx):
+                return False
+        return True   
