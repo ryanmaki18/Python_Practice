e8308ad39d0cb648375b45512d1e6415f6ccd769
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-31 00:12:00 -0700

    Finished one implementation of majorityElement. Will do more tomorrow.

--- majorityElement.py/majorityElement.py
+++ majorityElement.py/majorityElement.py
@@ -1,4 +1,11 @@
+import math
+
 class Solution:
+    # Using sorting method
     def majorityElement(self, nums: list[int]) -> int:
-        return None
+        majority_index = len(nums) // 2
+        nums.sort()
+        return nums[majority_index]
+    
+    # FIXME: Work on other implementations
     
