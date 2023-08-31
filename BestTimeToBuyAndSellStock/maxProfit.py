fb77551d533cb3a3de0c21b64a592722d1a542d7
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-30 18:10:03 -0700

    2 implementations of BestTimeToBuyAndSellStock

--- BestTimeToBuyAndSellStock/maxProfit.py
+++ BestTimeToBuyAndSellStock/maxProfit.py
@@ -1,3 +1,26 @@
 class Solution:
-    def maxProfit(self, prices: list[int]) -> int:
+    def maxProfit(self, prices):
+        profit = 0
+        buy = prices[0]
+        for price in prices[1:]:
+            if price > buy:
+                profit = max(profit, price - buy)
+            else:
+                buy = price
+        
+        return profit
+    
+    
+    # def maxProfit(self, prices: list[int]) -> int:
+    #     if not prices:
+    #         return 0
+        
+    #     min_price = prices[0]
+    #     max_profit = 0
+        
+    #     for price in prices:
+    #         min_price = min(min_price, price)
+    #         max_profit = max(max_profit, price - min_price)
+        
+    #     return max_profit
         
