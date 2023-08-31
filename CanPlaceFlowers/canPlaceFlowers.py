5cf0702a2bd8c416a05e014b15517a43bb2b4ad5
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-30 23:26:47 -0700

    finished

--- CanPlaceFlowers/canPlaceFlowers.py
+++ CanPlaceFlowers/canPlaceFlowers.py
@@ -1,18 +1,28 @@
 class Solution:
-    # Non-Greedy Approach
     # def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
-        
+    #     count, prev = 0, 0
 
+    #     for cur in flowerbed:
+    #         if cur == 1:
+    #             if prev == 1: # violation!
+    #                 count -= 1
+    #             prev = 1
+    #         else:
+    #             if prev == 1: # can't plant
+    #                 prev = 0 
+    #             else:
+    #                 count += 1
+    #                 prev = 1 # the cur plot gets taken
+            
+    #     return count >= n
 
-
-
-
+            
     # Greedy Approach
     def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
         c=0
         if n==0:
             return True
-            
+
         if len(flowerbed)==1:
             if flowerbed[0]==0:
                 return True
