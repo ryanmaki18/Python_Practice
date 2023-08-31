fc8bc1863e10a239709978972a2caacb21131fa4
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-30 18:28:39 -0700

    Finished greedy approach. Working on brute force approach

--- CanPlaceFlowers/canPlaceFlowers.py
+++ CanPlaceFlowers/canPlaceFlowers.py
@@ -1,4 +1,40 @@
 class Solution:
+    # Non-Greedy Approach
+    # def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
+        
+
+
+
+
+
+    # Greedy Approach
     def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
-        
-        
+        c=0
+        if n==0:
+            return True
+            
+        if len(flowerbed)==1:
+            if flowerbed[0]==0:
+                return True
+            else:
+                return False
+
+        for i in range(len(flowerbed)):
+            if i==0:
+                if flowerbed[i]==0 and flowerbed[i+1]!=1:
+                    flowerbed[i]=1
+                    c+=1
+            if i==len(flowerbed)-1:
+                if flowerbed[i]==0 and flowerbed[i-1]!=1:
+                    flowerbed[i]=1
+                    c+=1
+            if flowerbed[i]==1:
+                continue
+            else:
+                if flowerbed[i-1]!=1 and flowerbed[i+1]!=1:
+                    flowerbed[i]=1
+                    c+=1
+        if c>=n:
+            return True
+        else:
+            return False
--- CanPlaceFlowers/test_canPlaceFlowers.py
+++ CanPlaceFlowers/test_canPlaceFlowers.py
@@ -10,8 +10,20 @@ class CanPlaceFlowers(unittest.TestCase):
         
     def test_example_2(self):
         solution = Solution()
+        flowerbed = [1,0,0,0,0,0,1]
+        n = 2
+        self.assertTrue(solution.canPlaceFlowers(flowerbed, n))
+        
+    def test_example_3(self):
+        solution = Solution()
         flowerbed = [1,0,0,0,1]
         n = 2
+        self.assertFalse(solution.canPlaceFlowers(flowerbed, n))
+        
+    def test_example_4(self):
+        solution = Solution()
+        flowerbed = [1,0,0,0,0,0,1]
+        n = 3
         self.assertFalse(solution.canPlaceFlowers(flowerbed, n))
         
 if __name__ == '__main__':
