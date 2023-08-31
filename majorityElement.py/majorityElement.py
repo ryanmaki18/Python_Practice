d6e8748565ef69e1be49490559a1efa6ad6ec265
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-31 16:41:55 -0700

    found moore voting algorithm implementation

--- majorityElement.py/majorityElement.py
+++ majorityElement.py/majorityElement.py
@@ -1,4 +1,3 @@
-import math
 from collections import defaultdict
 
 class Solution:
@@ -7,18 +6,34 @@ class Solution:
     #     majority_index = len(nums) // 2
     #     nums.sort()
     #     return nums[majority_index]
-    
-    # Hash Map
-    def majorityElement(self, nums: list[int]) -> int:
-        m = defaultdict(int)
 
-        for num in nums:
-            m[num] += 1
+    # # Hash Map
+    # def majorityElement(self, nums: list[int]) -> int:
+    #     m = defaultdict(int)
 
-        n = len(nums) // 2
-        for key, value in m.items():
-            if value > n:
-                return key
+    #     for num in nums:
+    #         m[num] += 1
+
+    #     n = len(nums) // 2
+    #     for key, value in m.items():
+    #         if value > n:
+    #             return key
         
-        return 0
+    #     return 0
 
+    # Moore Voting Algorithm
+
+    def majorityElement(self, nums: list[int]) -> int:
+        count = 0
+        candidate = 0
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        
+        return candidate
