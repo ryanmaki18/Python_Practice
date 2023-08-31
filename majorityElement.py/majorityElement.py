96c70101cf0d61513e0aea57da69a96239b96e45
ryanmaki18 <ryanmannismaki@icloud.com>
2023-08-31 16:32:28 -0700

    implemented hash map

--- majorityElement.py/majorityElement.py
+++ majorityElement.py/majorityElement.py
@@ -1,11 +1,24 @@
 import math
+from collections import defaultdict
 
 class Solution:
-    # Using sorting method
+    # # Using sorting method
+    # def majorityElement(self, nums: list[int]) -> int:
+    #     majority_index = len(nums) // 2
+    #     nums.sort()
+    #     return nums[majority_index]
+    
+    # Hash Map
     def majorityElement(self, nums: list[int]) -> int:
-        majority_index = len(nums) // 2
-        nums.sort()
-        return nums[majority_index]
-    
-    # FIXME: Work on other implementations
-    
+        m = defaultdict(int)
+
+        for num in nums:
+            m[num] += 1
+
+        n = len(nums) // 2
+        for key, value in m.items():
+            if value > n:
+                return key
+        
+        return 0
+
