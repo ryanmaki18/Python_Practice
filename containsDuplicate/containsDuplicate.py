92bab15bcf7124aa8afc14779c0530ec614f84ba
ryanmaki18 <ryanmannismaki@icloud.com>
2023-09-02 23:32:08 -0700

    added another implementation

--- containsDuplicate/containsDuplicate.py
+++ containsDuplicate/containsDuplicate.py
@@ -24,5 +24,13 @@ class Solution:
     #         else:
     #             num_set[num] = num_set.get(num, 0) + 1
     #     return False
-        
+    
+        # Using Sorting Method
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        nums.sort()
+        n = len(nums)
+        for i in range(1, n):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False        
     
