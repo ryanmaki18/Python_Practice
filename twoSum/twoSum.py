c4d923dc38463feacd2ccde438e1b41667d60649
ryanmaki18 <ryanmannismaki@icloud.com>
2023-09-02 22:57:41 -0700

    Made another implementation

--- twoSum/twoSum.py
+++ twoSum/twoSum.py
@@ -1,14 +1,24 @@
 class Solution:
+    # Brute Force
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        hash_map = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in hash_map:
-                return [hash_map[complement], i]
-            else:
-                hash_map[num] = i
-        return []
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []  # No solution found
     
+    # # Using Hash Map
+    # def twoSum(self, nums: list[int], target: int) -> list[int]:
+    #     hash_map = {}
+    #     for i, num in enumerate(nums):
+    #         complement = target - num
+    #         if complement in hash_map:
+    #             return [hash_map[complement], i]
+    #         else:
+    #             hash_map[num] = i
+    #     return []
+        
     
 if __name__ == "__main__":
     solution = Solution()
