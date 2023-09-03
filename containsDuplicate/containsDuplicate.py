f2c262abb34fbe125ff816f1a9d96e3c059bb654
ryanmaki18 <ryanmannismaki@icloud.com>
2023-09-02 23:28:44 -0700

    Added implementations

--- containsDuplicate/containsDuplicate.py
+++ containsDuplicate/containsDuplicate.py
@@ -1,11 +1,28 @@
 class Solution:
+    # # Implementation using the Set() built-in. Is O(n)
+    # def containsDuplicate(self, nums: list[int]) -> bool:
+    #     num_set = set()
+    #     for num in nums:
+    #         if num in num_set:
+    #             return True
+    #         else:
+    #             num_set.add(num)
+    #     return False
+
+    # Better way of implementation if using a set. Much cleaner and easier
     def containsDuplicate(self, nums: list[int]) -> bool:
-        num_set = set()
-        for num in nums:
-            if num in num_set:
-                return True
-            else:
-                num_set.add(num)
-        return False
+       if len(nums) == len(set(nums)):
+           return False
+       return True
+
+    # #Implementation using Hash Map. Is O(n).
+    # def containsDuplicate(self, nums: list[int]) -> bool:
+    #     num_set = {}
+    #     for num in nums:
+    #         if num in num_set and num_set[num] >= 1:
+    #             return True
+    #         else:
+    #             num_set[num] = num_set.get(num, 0) + 1
+    #     return False
         
     
