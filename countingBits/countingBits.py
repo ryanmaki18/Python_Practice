6993a8357461add246aad2e4a33e522021547757
ryanmaki18 <ryanmannismaki@icloud.com>
2023-09-06 16:49:13 -0700

    Finished one implementation of countBits

--- countingBits/countingBits.py
+++ countingBits/countingBits.py
@@ -2,12 +2,17 @@
 # ... (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
 class Solution:
+    # Brute-Force Solution
     def countBits(self, n: int) -> list[int]:
         result = []
+        
         for i in range(0, n + 1):
             binary = bin(i)
-            print(binary[2:])
-
-            if binary[2:].contains(1):
-
-        return none
+            one_count = 0
+            
+            for num in binary[2:]:
+                if num == '1':
+                    one_count += 1
+            result.append(one_count)
+        
+        return result
